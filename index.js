10e66cc9a5eaa886faabdbeaeffa16e8573b8a33
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-03 20:18:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const io  = new Server(server, {
         methods: ["GET", "POST"],
     },
 });
-
+// console.log(data);
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
