9e3a8b3cf9a402b62db34db1fef326943efb02c8
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-03 20:41:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-app.use(cors());
+app.use(cors());   
 
 const server =  http.createServer(app);
 
