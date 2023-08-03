8fef2611cc13d39dfae2b5de31ccd039307f8523
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-03 20:25:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
 
 
     socket.on("join_room", (data) => {
-     //   socket.join(data);
+        socket.join(data);
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
     socket.on("send_message", (data) => {
